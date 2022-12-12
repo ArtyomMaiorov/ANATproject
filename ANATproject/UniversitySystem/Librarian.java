786d500166968d14d65a97ffcd66eb99c2ee753f
nurbek0316 <maulethan.nm@gmail.com>
2022-12-13 01:15:08 +0600

    tut tozhe udalil kommenti poka koda net

--- ANATproject/UniversitySystem/Librarian.java
+++ ANATproject/UniversitySystem/Librarian.java
@@ -1,68 +1,32 @@
 package ANAT;
 
-
-/**
-* @generated
-*/
 public class Librarian extends Employee {
     
-    /**
-    * @generated
-    */
     private Librarian tittle;
-    /**
-    * @generated
-    */
     private Database database;
-    
-    /**
-    * @generated
-    */
     private Book book;
-    
-    
 
-    /**
-    * @generated
-    */
     private Librarian getTittle() {
         return this.tittle;
     }
     
-    /**
-    * @generated
-    */
     private Librarian setTittle(Librarian tittle) {
         this.tittle = tittle;
     }
-    
-    
-    
-    /**
-    * @generated
-    */
+
     public Book getBook() {
         return this.book;
     }
     
-    /**
-    * @generated
-    */
+   
     public Book setBook(Book book) {
         this.book = book;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     public Database getDatabase() {
         return this.database;
     }
     
-    /**
-    * @generated
-    */
     public Database setDatabase(Database database) {
         this.database = database;
     }
@@ -72,37 +36,27 @@ public class Librarian extends Employee {
 
     //                          Operations                                  
     
-    /**
-    * @generated
-    */
+   
     public boolean checkID() {
         //TODO
         return false;
     }
     
-    /**
-    * @generated
-    */
+   
     public boolean retrieveBook() {
         //TODO
         return false;
     }
     
-    /**
-    * @generated
-    */
+   
     public boolean checkID(ID)() {
         //TODO
         return false;
     }
     
-    /**
-    * @generated
-    */
+   
     public boolean checkID() {
         //TODO
         return false;
     }
-    
-    
 }
--- ANATproject/UniversitySystem/Mark.java
+++ ANATproject/UniversitySystem/Mark.java
@@ -1,330 +1,148 @@
 package ANAT;
 
-
-/**
-* @generated
-*/
 public class Mark {
-    
-    /**
-    * @generated
-    */
     private Course courseName;
-    
-    /**
-    * @generated
-    */
     private double points;
-    
-    /**
-    * @generated
-    */
     private double firstAtt;
-    
-    /**
-    * @generated
-    */
     private double secondAtt;
-    
-    /**
-    * @generated
-    */
     private double finalScore;
-    
-    /**
-    * @generated
-    */
     private double totalAtt;
-    
-    /**
-    * @generated
-    */
     private String letterGrade;
-    
-    
-    /**
-    * @generated
-    */
     private Teacher teacher;
-    
-    /**
-    * @generated
-    */
     private Teacher teacher;
-    
-    /**
-    * @generated
-    */
     private Teacher teacher;
-    
-    /**
-    * @generated
-    */
     private Teacher teacher;
-    
-    /**
-    * @generated
-    */
     private Database database;
-    
-    /**
-    * @generated
-    */
     private Database database;
-    
-    /**
-    * @generated
-    */
     private Student student;
-    
-    /**
-    * @generated
-    */
     private Student student;
-    
-    /**
-    * @generated
-    */
     private Student student;
-    
-    
 
-    /**
-    * @generated
-    */
+    
     private Course getCourseName() {
         return this.courseName;
     }
     
-    /**
-    * @generated
-    */
     private Course setCourseName(Course courseName) {
         this.courseName = courseName;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private double getPoints() {
         return this.points;
     }
-    
-    /**
-    * @generated
-    */
+
     private double setPoints(Real points) {
         this.points = points;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private double getFirstAtt() {
         return this.firstAtt;
     }
-    
-    /**
-    * @generated
-    */
+
     private double setFirstAtt(Real firstAtt) {
         this.firstAtt = firstAtt;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private double getSecondAtt() {
         return this.secondAtt;
     }
-    
-    /**
-    * @generated
-    */
+
     private double setSecondAtt(Real secondAtt) {
         this.secondAtt = secondAtt;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private double getFinalScore() {
         return this.finalScore;
     }
-    
-    /**
-    * @generated
-    */
+
     private double setFinalScore(Real finalScore) {
         this.finalScore = finalScore;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private double getTotalAtt() {
         return this.totalAtt;
     }
-    
-    /**
-    * @generated
-    */
+
     private double setTotalAtt(Real totalAtt) {
         this.totalAtt = totalAtt;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private String getLetterGrade() {
         return this.letterGrade;
     }
-    
-    /**
-    * @generated
-    */
+
     private String setLetterGrade(String letterGrade) {
         this.letterGrade = letterGrade;
     }
-    
-    
-    
-    /**
-    * @generated
-    */
+
     public Teacher getTeacher() {
         return this.teacher;
     }
-    
-    /**
-    * @generated
-    */
+
+    public Teacher setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Teacher getTeacher() {
+        return this.teacher;
+    }
+
     public Teacher setTeacher(Teacher teacher) {
         this.teacher = teacher;
     }
     
-    
-    /**
-    * @generated
-    */
     public Teacher getTeacher() {
         return this.teacher;
     }
-    
-    /**
-    * @generated
-    */
+
     public Teacher setTeacher(Teacher teacher) {
         this.teacher = teacher;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     public Teacher getTeacher() {
         return this.teacher;
     }
-    
-    /**
-    * @generated
-    */
+
     public Teacher setTeacher(Teacher teacher) {
         this.teacher = teacher;
     }
-    
-    
-    /**
-    * @generated
-    */
-    public Teacher getTeacher() {
-        return this.teacher;
-    }
-    
-    /**
-    * @generated
-    */
-    public Teacher setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
-    
-    
-    /**
-    * @generated
-    */
+
     public Student getStudent() {
         return this.student;
     }
-    
-    /**
-    * @generated
-    */
+
     public Student setStudent(Student student) {
         this.student = student;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     public Student getStudent() {
         return this.student;
     }
-    
-    /**
-    * @generated
-    */
+
     public Student setStudent(Student student) {
         this.student = student;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     public Student getStudent() {
         return this.student;
     }
-    
-    /**
-    * @generated
-    */
+
     public Student setStudent(Student student) {
         this.student = student;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     public Database getDatabase() {
         return this.database;
     }
-    
-    /**
-    * @generated
-    */
+
     public Database setDatabase(Database database) {
         this.database = database;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     public Database getDatabase() {
         return this.database;
     }
-    
-    /**
-    * @generated
-    */
+
     public Database setDatabase(Database database) {
         this.database = database;
     }
@@ -333,26 +151,17 @@ public class Mark {
     
 
     //                          Operations                                  
-    
-    /**
-    * @generated
-    */
+
     public String convertToNumber() {
         //TODO
         return "";
     }
-    
-    /**
-    * @generated
-    */
+
     public double getTotalPoints() {
         //TODO
         return 0.0;
     }
-    
-    /**
-    * @generated
-    */
+
     public String getLetterScore() {
         //TODO
         return "";
