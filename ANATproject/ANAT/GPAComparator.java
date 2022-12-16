921b76be2f4248a2d084d84a5790f5f576ce8c51
Томирис <@LAPTOP-C2PH1CIU>
2022-12-16 14:53:10 +0600

    t

--- ANATproject/ANAT/GPAComparator.java
+++ ANATproject/ANAT/GPAComparator.java
@@ -1,4 +1,7 @@
 package ANAT;
+
+import java.util.Comparator;
+
 public class GPAComparator implements Comparator<Student> {
 	public int compare(Student o1, Student o2) {
 		return o1.getGPA().compareTo(o2.getGPA());
